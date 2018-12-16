8c0778c57974b1c20e1d15847e4b429c2defc372
Michael Morgan <michael1986morgan@gmail.com>
2018-12-15 19:26:02 -0500

    realestate app init

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './sass/main.scss';
+
+import Header from './components/Header';
+import Filter from './components/Filter';
+import Listings from './components/Listings';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Header />
+        <section id="content-area">
+          <Filter />
+          <Listings />
+        </section>
       </div>
-    );
+    )
   }
 }
 
---
+++ src/components/Listings.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Listings extends Component {
+  render() {
+    return (
+      <section className="listings">
+        Listings
+      </section>
+    )
+  }
+}
+
+export default Listings;
